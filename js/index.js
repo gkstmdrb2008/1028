e7d8ea38f73c7b0d54287f6fd753387ffeb926c4
gkstmdrb2008 <62547332+gkstmdrb2008@users.noreply.github.com>
2020-10-28 16:19:36 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,153 @@
+$(document).ready(function () {
+    $(".box1").on("mouseover", function () {
+        $(".box19").addClass("active")
+    })
+    $(".box1").on("mouseout", function () {
+        $(".box19").removeClass("active")
+    })
+
+
+    $(".box2").on("mouseover", function () {
+        $(".box18").addClass("active")
+    })
+    $(".box2").on("mouseout", function () {
+        $(".box18").removeClass("active")
+    })
+
+
+    $(".box3").on("mouseover", function () {
+        $(".box17").addClass("active")
+    })
+    $(".box3").on("mouseout", function () {
+        $(".box17").removeClass("active")
+    })
+
+
+    $(".box4").on("mouseover", function () {
+        $(".box16").addClass("active")
+    })
+    $(".box4").on("mouseout", function () {
+        $(".box16").removeClass("active")
+    })
+
+
+    $(".box5").on("mouseover", function () {
+        $(".box15").addClass("active")
+    })
+    $(".box5").on("mouseout", function () {
+        $(".box15").removeClass("active")
+    })
+
+
+    $(".box6").on("mouseover", function () {
+        $(".box14").addClass("active")
+    })
+    $(".box6").on("mouseout", function () {
+        $(".box14").removeClass("active")
+    })
+
+
+    $(".box7").on("mouseover", function () {
+        $(".box13").addClass("active")
+    })
+    $(".box7").on("mouseout", function () {
+        $(".box13").removeClass("active")
+    })
+
+
+    $(".box8").on("mouseover", function () {
+        $(".box12").addClass("active")
+    })
+    $(".box8").on("mouseout", function () {
+        $(".box12").removeClass("active")
+    })
+
+
+    $(".box9").on("mouseover", function () {
+        $(".box11").addClass("active")
+    })
+    $(".box9").on("mouseout", function () {
+        $(".box11").removeClass("active")
+    })
+
+
+    $(".box0").on("mouseover", function () {
+        $(".box9").addClass("active")
+    })
+    $(".box0").on("mouseout", function () {
+        $(".box9").removeClass("active")
+    })
+
+
+    $(".box11").on("mouseover", function () {
+        $(".box8").addClass("active")
+    })
+    $(".box11").on("mouseout", function () {
+        $(".box8").removeClass("active")
+    })
+
+
+    $(".box12").on("mouseover", function () {
+        $(".box7").addClass("active")
+    })
+    $(".box12").on("mouseout", function () {
+        $(".box7").removeClass("active")
+    })
+
+
+    $(".box13").on("mouseover", function () {
+        $(".box6").addClass("active")
+    })
+    $(".box13").on("mouseout", function () {
+        $(".box6").removeClass("active")
+    })
+
+
+    $(".box14").on("mouseover", function () {
+        $(".box5").addClass("active")
+    })
+    $(".box14").on("mouseout", function () {
+        $(".box5").removeClass("active")
+    })
+
+
+    $(".box15").on("mouseover", function () {
+        $(".box4").addClass("active")
+    })
+    $(".box15").on("mouseout", function () {
+        $(".box4").removeClass("active")
+    })
+
+
+    $(".box16").on("mouseover", function () {
+        $(".box3").addClass("active")
+    })
+    $(".box16").on("mouseout", function () {
+        $(".box3").removeClass("active")
+    })
+
+
+    $(".box17").on("mouseover", function () {
+        $(".box2").addClass("active")
+    })
+    $(".box17").on("mouseout", function () {
+        $(".box2").removeClass("active")
+    })
+
+
+    $(".box18").on("mouseover", function () {
+        $(".box1").addClass("active")
+    })
+    $(".box18").on("mouseout", function () {
+        $(".box1").removeClass("active")
+    })
+
+
+    $(".box19").on("mouseover", function () {
+        $(".box0").addClass("active")
+    })
+    $(".box19").on("mouseout", function () {
+        $(".box0").removeClass("active")
+    })
+})
+
